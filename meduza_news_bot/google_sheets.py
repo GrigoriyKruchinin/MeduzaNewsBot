01756fd8ca705c16fb19d72b2c07d23e68498dd3
GrigoriyKruchinin <gkruchinin75@gmail.com>
2024-04-15 16:33:39 +0200

    added docs for google_sheets.py

--- meduza_news_bot/google_sheets.py
+++ meduza_news_bot/google_sheets.py
@@ -12,6 +12,17 @@ DELAY_SECONDS = 60
 
 
 def authenticate(credentials_file, scope=None):
+    """
+    Аутентифицирует сервисный аккаунт и возвращает объект клиента
+    для взаимодействия с Google Sheets API.
+
+    Args:
+        credentials_file (str): Путь к файлу с учетными данными для аутентификации.
+        scope (list, optional): Список разрешений OAuth 2.0.
+
+    Returns:
+        gspread.client.Client: Объект клиента для взаимодействия с Google Sheets API.
+    """
     if scope is None:
         scope = [
             "https://spreadsheets.google.com/feeds",
@@ -26,21 +37,36 @@ def authenticate(credentials_file, scope=None):
 
 
 def open_worksheet(client, sheet_name):
+    """
+    Открывает Google Таблицу по названию. Записывает заголовки для колонок.
+
+    Args:
+        client (gspread.client.Client): Объект клиента для взаимодействия с Google API.
+        sheet_name (str): Название листа Google Таблицы.
+    Returns:
+        gspread.models.Worksheet: Объект листа Google Таблицы.
+    """
     # Открыть Google Таблицу по названию
     sheet = client.open(sheet_name)
 
     # Получить объект листа
     worksheet = sheet.get_worksheet(0)  # 0 для первого листа
 
-    # Если таблица пустая, записать заголовки
+    # Если таблица пустая, записать заголовки для колонок
     if not worksheet.col_values(1):
-        # Записать структуру данных
         worksheet.update_cell(1, 1, "Время занесения")
         worksheet.update_cell(1, 2, "Заголовок")
     return worksheet
 
 
 def write_data_to_worksheet(worksheet, data):
+    """
+    Записывает данные в Google Таблицу.
+
+    Args:
+        worksheet: Объект листа Google Таблицы, в который будут записываться данные.
+        data: Список данных, которые будут записаны в таблицу.
+    """
     # Начать запись данных со следующей строки
     next_row = len(worksheet.col_values(1)) + 1
 
@@ -61,6 +87,16 @@ def write_data_to_worksheet(worksheet, data):
 
 
 def write_to_google_sheet(data, sheet_name, credentials_file):
+    """
+    Аутентифицирует сервисный аккаунт, открывает Google Таблицу
+    и записывает в нее данные.
+
+    Args:
+        data: Список данных, которые будут записаны в таблицу.
+        sheet_name: Название листа Google Таблицы, куда будут записываться данные.
+        credentials_file: Путь к файлу с учетными данными для аутентификации.
+        url: URL, откуда были получены данные (для записи в заголовок таблицы).
+    """
     client = authenticate(credentials_file)
     worksheet = open_worksheet(client, sheet_name)
     write_data_to_worksheet(worksheet, data)
