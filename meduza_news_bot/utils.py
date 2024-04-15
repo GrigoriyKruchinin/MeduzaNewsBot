07daa872bf6c4c391c977e6d2f54e958f6e0168c
GrigoriyKruchinin <gkruchinin75@gmail.com>
2024-04-15 09:14:09 +0200

    added html_parser and utils for it

--- meduza_news_bot/utils.py
+++ meduza_news_bot/utils.py
@@ -1,19 +1,6 @@
 def sanitize_string(string):
-    """
-    Очищает строку от лишних пробелов и символов.
-
-    Args:
-        text (str): Строка для очистки.
-
-    Returns:
-        str: Очищенная строка.
-    """
     return string.strip().replace("\xa0", " ")
 
 
 class ParsingError(Exception):
-    """
-    Исключение, возникающее при ошибке парсинга данных.
-    """
-
     pass
